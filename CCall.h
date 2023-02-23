5aecb733337dabbc99f3b7cfe6666b059093b94a
Oktawn <71918550+Oktawn@users.noreply.github.com>
2023-02-23 19:02:14 +0500

    operator copy

--- CCall.h
+++ CCall.h
@@ -4,7 +4,6 @@
 #include <chrono>
 using namespace std::chrono;
 
-
 class CCall
 {
 private:
@@ -17,17 +16,18 @@ private:
 	steady_clock::time_point start_number;
 
 	static int number_key_call;
+	CCall &operator=(const CCall &) { return *this; }
 
 public:
 	CCall(bool priority,
-		std::string abonent = "anonim",
-		std::string numbers = "79XXXXXXXXXX",
-		std::string thems = "programming");
+		  std::string abonent = "anonim",
+		  std::string numbers = "79XXXXXXXXXX",
+		  std::string thems = "programming");
 	CCall();
 
 	bool correct_names(std::string);
 	bool correct_number(std::string);
-	bool correct_priority(const bool&);
+	bool correct_priority(const bool &);
 
 	void show_thems() const;
 	void show_number() const;
@@ -39,16 +39,15 @@ public:
 	void change_thems(std::string, bool);
 	void change_number(std::string);
 	void change_abonent(std::string);
-	void change_priority(const bool&);
+	void change_priority(const bool &);
 
 	float time_call();
 
-	friend const bool operator>(CCall&, CCall&);
-	friend const bool operator==(CCall&, CCall&);
+	friend const bool operator>(CCall &, CCall &);
+	friend const bool operator==(CCall &, CCall &);
 
 	static int show_key()
 	{
 		return number_key_call;
 	}
 };
-
