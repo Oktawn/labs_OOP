d34b1145279934598ceeb01608985dbfe105de61
Oktawn <71918550+Oktawn@users.noreply.github.com>
2023-05-03 18:35:49 +0500

    dly tvari

--- labs.hpp
+++ labs.hpp
@@ -2,14 +2,16 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <numeric>
+#include <iterator>
 
 #include <list>
-#include "CCall.hpp"
+#include "CCall.cpp"
 
 #include <map>
 #include "Newpaper.hpp"
 
-#define set_map int, Newpaper,std::less<int>
+#define set_map int, Newpaper, std::less<int>
 
 std::map<set_map>::iterator it_m;
 
@@ -17,13 +19,19 @@ void create_v(std::vector<int> &v)
 {
     srand((int)time(nullptr));
     v.resize(rand() % 20);
-    for (auto &i : v)
-        i = rand() % 150 + (-50);
+    auto gen = []()
+    { return (rand() % 150 + (-50)); };
+    std::generate(v.begin(), v.end(), gen);
+    // for (auto &i : v)
+    //     i = rand() % 150 + (-50);
 }
 
 void zero_after_neg(const std::vector<int> &vec)
 {
     int zero = 0;
+
+    // for (auto &i : vec)
+    //   zero+=((i)<0 && (i+sizeof(i))==0) ? 1 : 0;
     for (int i(1); i < vec.size(); i++)
         zero += (vec[i - 1] < 0 && vec[i] == 0) ? 1 : 0;
     std::cout << zero << "\n";
@@ -40,4 +48,12 @@ void mul_pos(const std::vector<int> &vec)
             break;
     }
     std::cout << d << "\n";
+};
+
+void compres_vec(std::vector<int> &vec, int k)
+{
+    vec.erase(std::remove_if(vec.begin(), vec.end(),
+                             [&](int n)
+                             { return n % k == 0; }),
+              vec.end());
 };
--- main.cpp
+++ main.cpp
@@ -4,9 +4,16 @@ int main()
 {
 
     std::vector<int> vec;
+
     create_v(vec);
+    std::copy(vec.begin(), vec.end(), std::ostream_iterator<int, char>(std::cout, " "));
+    std::cout << "\nzero:";
     zero_after_neg(vec);
+    std::cout << "mul: ";
     mul_pos(vec);
+    compres_vec(vec, 5);
+    std::copy(vec.begin(), vec.end(), std::ostream_iterator<int, char>(std::cout, " "));
+    std::cout << "\n";
 
     std::list<CCall> call;
 
@@ -17,9 +24,10 @@ int main()
     Newpaper pap;
 
     std::map<set_map> map_pap;
-    map_pap[555]=pap;
+    map_pap[555] = pap;
+    
     it_m = map_pap.begin();
-    std::cout<< (*it_m).second << "\n";
-
+    std::cout << (*it_m).second << "\n";
+    system("pause>nul");
     return 0;
 }
